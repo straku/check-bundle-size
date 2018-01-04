0990dff4ef3a3cbd6f0e81f4e5edce0d36abd92d
Łukasz Strączyński <l.straczynski@hotmail.com>
2018-01-04 23:22:45 +0100

    Initial commit

---
+++ src/bin/check-bundle-size.js
@@ -0,0 +1,23 @@
+const minimist = require("minimist");
+
+const { measure } = require("../main");
+
+const description = `
+check-bundle-size [options]
+
+  Example usage: check-bundle-size --pattern='lib/*.js' --task='npm run build'
+
+Options:
+
+  -h, --help   Usage information
+  --pattern    Glob pattern for files to measure, example: 'lib/*.js'
+  --task       Task to run, example: 'npm run build'
+`;
+
+const argv = minimist(process.argv.slice(2));
+
+if (argv.pattern && argv.task) {
+  measure(argv.pattern, argv.task);
+} else {
+  console.log(description);
+}
---
+++ src/main.js
@@ -0,0 +1,65 @@
+const { readFileSync } = require("fs");
+const { execSync } = require("child_process");
+
+const glob = require("glob");
+const gzipSize = require("gzip-size");
+
+const { printTable } = require("./printer");
+
+function formatSize(size) {
+  const ranges = ["B", "kB", "MB", "GB"];
+  const rangeIndex = Math.floor(Math.abs(size) / 1024);
+  const divider = rangeIndex * 1024 || 1;
+  const formattedSize = (size / divider).toFixed(rangeIndex ? 2 : 0);
+  return `${formattedSize} ${ranges[rangeIndex]}`;
+}
+
+function readFiles(pattern) {
+  return glob.sync(pattern).map(file => {
+    const buffer = readFileSync(file);
+    return {
+      name: file.split("/")[1],
+      size: gzipSize.sync(buffer),
+    };
+  });
+}
+
+function getResults(filesBefore, filesAfter) {
+  return filesAfter.reduce((result, file) => {
+    const match = filesBefore.find(({ name }) => name === file.name);
+    result.push({
+      name: file.name,
+      sizeBefore: match ? formatSize(match.size) : 0,
+      sizeAfter: formatSize(file.size),
+      diff: formatSize(file.size - match ? match.size : 0),
+    });
+    return result;
+  }, []);
+}
+
+const TABLE_HEADER = [
+  { key: "name", label: "file name", align: "left" },
+  { key: "sizeBefore", label: "size old", align: "right" },
+  { key: "sizeAfter", label: "size new", align: "right" },
+  { key: "diff", label: "diff", align: "right" },
+];
+
+function measure(pattern, task) {
+  // check file sizes before build
+  const filesBefore = readFiles(pattern);
+  // clean and build project
+  execSync(task);
+  // check file sizes after build
+  const filesAfter = readFiles(pattern);
+
+  const result = getResults(filesBefore, filesAfter);
+
+  const table = {
+    header: TABLE_HEADER,
+    body: result,
+  };
+
+  printTable(table);
+}
+
+exports.measure = measure;
